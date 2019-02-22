d82dacbe94f47b6c3a3497004bff1d361de57c64
Daniel Maziad <dmaziad@gmail.com>
2019-02-22 11:14:47 -0500

    proxy rendering component on local machine

--- proxy/server.js
+++ proxy/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3060;
+const cors = require('cors');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
